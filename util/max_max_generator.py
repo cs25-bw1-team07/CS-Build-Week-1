4daeacf3245c1dd500959f5c8acab74f547db16f
Jason Sonnichsen <Harliquinn@gmail.com>
2020-02-04 02:28:54 -0500

    Random walk with collision detection TODO include branching

--- util/max_max_generator.py
+++ util/max_max_generator.py
@@ -4,7 +4,7 @@ class World:
         self.width = 0
         self.height = 0
     def generate_rooms(self, size_x, size_y, num_rooms):
-        random.seed('9Lambda1Thunder1D0me9')
+        # random.seed('9Lambda1Thunder1D0me9!')
         '''
         Fill up the grid, bottom to top, in a zig-zag pattern
         '''
@@ -20,42 +20,44 @@ class World:
         last_x=None
         last_y=4
         room_count = 0
-        # # Start generating rooms to the North
-        direction = 1  # 1: North, 2: West, 3: East, 4:South
-        # # While there are rooms to be created...
+        direction = 0  # 1: North, 2: East, 3: South, 4:West
         previous_room = None
-        vertical=[1,4]
-        horizontal=[2,3]
+        vertical=[0,2]
+        horizontal=[1,3]
+        #check what diretions are possible
         def get_dirs(x,y):
             retval=[]
-            if self.grid[y+1][x]==None:
-                if y < len(self.grid) - 1:
-                    retval.append(1)
-            if self.grid[y][x-1]==None:
-                if x > 0:
-                    retval.append(2)
-            if self.grid[y][x+1]==None:
-                if x < len(self.grid[0])-1:
-                    retval.append(3)
-            if self.grid[y-1][x]==None:
-                if y>0:
-                    retval.append(4)
-                return retval
+            if y < len(self.grid) - 1 and self.grid[y+1][x]==None:
+                retval.append(0)
+            if x > 0 and self.grid[y][x-1]==None:
+                retval.append(3)
+            if x < len(self.grid[0])-1 and self.grid[y][x+1]==None:
+                retval.append(1)
+            if y>0 and self.grid[y-1][x]==None:
+                retval.append(2)
+            return retval
+        # # While there are rooms to be created...
         while room_count < num_rooms:
-            print("ROOM COUNT",room_count)
+            # print("ROOM COUNT",room_count)
             # Calculate the direction of the room to be created
             if room_count>0:
                 available_dir=get_dirs(x,y)
-                print("Avail",available_dir)
-                new_direction=random.randint(1, 4)
+                # print("Avail",available_dir)
+                new_direction=random.randint(0, 3)
+                # print("New Dir",new_direction)
                 if available_dir==[]:
                     break
                 else:
                     while new_direction not in available_dir:
-                        if new_direction in horizontal:
-                            new_direction=tuple(horizontal)[new_direction==horizontal[0]]
-                        if new_direction in vertical:
-                            new_direction=tuple(horizontal)[new_direction==horizontal[0]]
+                        if new_direction==0:
+                            new_direction=(new_direction+2)%4
+                        elif new_direction==1:
+                            new_direction=(new_direction+2)%4
+                        elif new_direction==2:
+                            new_direction=(new_direction+2)%4
+                        elif new_direction==3:
+                            new_direction=(new_direction+2)%4
+                        new_direction=random.randint(0, 3)
                 # print("RAND",new_direction)
                 if direction in vertical and new_direction in vertical:
                     pass
@@ -68,16 +70,16 @@ class World:
                         last_y=new_direction
                     direction=new_direction
                 # print("New",direction,last_x,last_y)
-            if direction == 1 and y < size_y - 1:
+            if direction == 0 and y < size_y - 1:
                 room_direction = "n"
                 y += 1
-            elif direction == 2 and x > 0:
+            elif direction == 3 and x > 0:
                 room_direction = "w"
                 x -= 1
-            elif direction == 3 and x < size_x-1:
+            elif direction == 1 and x < size_x-1:
                 room_direction = "e"
                 x += 1
-            elif direction == 4 and y > 0:
+            elif direction == 2 and y > 0:
                 room_direction = "s"
                 y -= 1
             else:
@@ -90,7 +92,7 @@ class World:
             # Save the room in the World grid
             self.grid[y][x] = room
             # Connect the new room to the previous room
-            print("ROOM",previous_room,room,room_direction)
+            # print("ROOM",previous_room,room,room_direction)
             if previous_room is not None:
                 previous_room.connect_rooms(room, room_direction)
             previous_room = room
