2a9a12b750c88f922582d30293a980c2b0abe31c
kyle Guerrero <kyleguerrero85@gmail.com>
2020-02-04 09:25:54 -0600

    Added in a default false value to debug for prod env

--- adv_project/settings.py
+++ adv_project/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG") or config('DEBUG', cast=bool)
+DEBUG = os.getenv("DEBUG", False) or config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = []
 
